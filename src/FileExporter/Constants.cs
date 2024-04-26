d46bda3b3c389eb6e844078ed0e1dd89872b87f7
MnatsakanMargaryan <MnatsakanMargaryan@MNATS>
2024-04-26 15:58:28 +0400

    hard codes removed

--- src/FileExporter/Constants.cs
+++ src/FileExporter/Constants.cs
@@ -5,9 +5,9 @@ namespace FileExporter;
 public class Constants
 {
     public const int NameLength = 30;
-    public const int CsvLinesCount = 1_00; //todo :: change
-    public const int ExcelLinesCount = 1_0; //todo :: change
-    public const int PdfLinesCount = 100; //todo :: change
+    public const int CsvLinesCount = 1_000_000;
+    public const int ExcelLinesCount = 1_000_000;
+    public const int PdfLinesCount = 10_000; 
     public const int FileMaxSizeInBytes = 10 * 1024 * 1024; //10 MB
     public const string DateTimePlaceHolder = "{DateTime}";
     public const string DefaultFontName = "Arial";
